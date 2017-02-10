031dcdebdec7cd5154d9e43e2d027f7d445dff4b
Mark Tattrie <MTattrie93@gmail.com>
2017-02-09 20:42:47 -0800

    updated template.php

--- application/views/template.php
+++ application/views/template.php
@@ -1,17 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="utf-8">
-		<title>{pagetitle}</title>
-        <meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <link rel="stylesheet" type="text/css" href="/assets/css/default.css"/>
+            <meta charset="utf-8">
+            <title>{pagetitle}</title>
+            <meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <link rel="stylesheet" type="text/css" media="all" href="/assets/css/reset.css" />
+            <link rel="stylesheet" type="text/css" media="all" href="/assets/css/text.css" />
+            <link rel="stylesheet" type="text/css" media="all" href="/assets/css/style.css" />
+            <link rel="stylesheet" type="text/css" media="all" href="/assets/css/lightbox.css"/>
+            <link rel="stylesheet" type="text/css" media="all" href="/assets/css/screen.css"/>
 	</head>
 	<body>
-        <div id="container">
-			{content}
-			<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. 
-				{ci_version}</p>
-        </div>
+            <div id="wrapper">
+                <div id="header">
+                    <span class="myhead">UGLIBOI-Z</span>
+                    <span class="mynav">
+                        <ul>
+                            <li><a href="/">Home</li>
+                            <li><a href="/">Parts</li>
+                            <li><a href="/">Assembly</li>
+                            <li><a href="/">History</li>
+                            <li><a href="/">About</li>
+                        </ul>
+                    </span>
+                </div>
+                <div class="alone"></div>
+			<div id="content">
+                            {content}
+			</div>
+                <div id="footer" class="span12">UgliBoi-z</div>
+            </div>
 	</body>
+        <script type="text/javascript" src="/js/jquery-1.11.0.min.js"></script> 
+        <script type="text/javascript" src="/js/lightbox.min.js"></script>  
 </html>
