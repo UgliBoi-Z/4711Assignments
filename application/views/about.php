ab48b6133c4cdcd8c22455100f0f10ad1ed5951a
Jacob Mcphail <jacobmcphail@gmail.com>
2017-03-30 19:05:35 -0700

    Assembly Functional

--- application/views/about.php
+++ application/views/about.php
@@ -1,12 +1,13 @@
 <h1> About UgliBoy-Z</h1>
 <p>UgliBoi-Z factory is a fictional robot factory
     created for an academic project and is not related to or endorsed
-    by any similar business in the real world</p>
-<table>
-    <tr>Members include:</tr>
-    <tr><td>Justen DePourcq</td></tr>
-    <tr><td>Jordan Lin</td></tr>
-    <tr><td>Jacob Mcphail</td></tr>
-    <tr><td>Mark Chen</td></tr>
-    <tr><td>Mark Tattrie</td></tr>
-</table>
+    by any similar business in the real world</p><br>
+<h3 class="myhead">Members include:</h3>
+<h3 class="myhead">Jacob Mcphail</h3>
+<h3 class="myhead">Justen DePourcq</h3>
+<h3 class="myhead">Jordan Lin</h3>
+<h3 class="myhead">Mark Chen</h3>
+<h3 class="myhead">Mark Tattrie</h3>
+<br>
+
+
