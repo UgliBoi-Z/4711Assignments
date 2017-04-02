0895b231f904ad4f37ab916963acd91d211ca7e0
YiHChen <parka415@hotmail.com>
2017-04-02 15:23:00 -0700

    Add role functionality with dropdown

--- application/views/template.php
+++ application/views/template.php
@@ -11,21 +11,22 @@
 		<link rel="stylesheet" type="text/css" media="all" href="/assets/css/lightbox.css"/>
 		<link rel="stylesheet" type="text/css" media="all" href="/assets/css/screen.css"/>
 		<link rel="stylesheet" type="text/css" media="all" href="/assets/css/homepage.css"/>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
         <script type="text/javascript" src="/js/lightbox.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	</head>
 	<body>
-            <div id="wrapper">
-                <div id="header">
-                    <span class="myhead">UGLIBOI-Z</span>
-                    <span class="mynav">{menubar}</span>
-                </div>
-				<div id="content">
-	                            {content}
-				</div>
-                <div id="footer" class="span12">UgliBoi-z</div>
-            </div>
+		<div id="wrapper">
+			<div id="header">
+				<span class="myhead">UGLIBOI-Z</span>
+				<span class="mynav">{menubar}</span>
+			</div>
+			<div id="content">
+				{content}
+			</div>
+			<div id="footer" class="span12">UgliBoi-z</div>
+		</div>
 	</body>
 	
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </html>
