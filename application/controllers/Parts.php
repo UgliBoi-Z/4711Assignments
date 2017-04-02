45df1dbc146cf9ca432e77cc005f8bb8739bb378
Mark Tattrie <MTattrie93@gmail.com>
2017-04-02 16:11:44 -0700

    fixed parts page to use database

--- application/controllers/Parts.php
+++ application/controllers/Parts.php
@@ -2,66 +2,113 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Parts extends Application
-{
+class Parts extends Application {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/
-	 * 	- or -
-	 * 		http://example.com/welcome/index
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-	    $this->load->database();
-	    $query = $this->db->query('SELECT * FROM parts');
-            $parts = $query->result_array()
-            $this->load->library('table');
-            $parms = array(
-                'table_open' => '<table class="gallery">',
-                'cell_start' => '<td class="oneimage">',
-                'cell_alt_start' => '<td calss="oneimage">'
+    /**
+     * index for Parts page. Collects all records from the robots and parts models
+     */
+    public function index() {
+        $this->data['pagebody'] = 'parts';
+        $role = $this->session->userdata('userrole');
+        if ((strcmp($role, "Worker") != 0) &&
+                (strcmp($role, "Boss") != 0) &&
+                (strcmp($role, "Supervisor") != 0)) {
+            redirect($_SERVER['HTTP_REFERER']); // back where we came from
+        }
+
+        $this->load->database();
+        $query = $this->db->query('SELECT * FROM parts');
+        $parts = $query->result_array();
+
+        $this->load->library('table');
+
+        $parms = array(
+            'table_open' => '<table class = "gallery">',
+            'cell_start' => '<td class = "oneimage">',
+            'cell_alt_start' => '<td class = "oneimage">'
+        );
+
+        $this->table->set_template($parms);
+
+        $part_heads = array();
+        $part_torsos = array();
+        $part_feet = array();
+        foreach ($parts as $part) {
+            if ($part['part_code'] == "a1" || $part['part_code'] == "b1" || $part['part_code'] == "c1") {
+                $part_heads[] = array('part_id' => $part['part_id'],
+                    'part_code' => $part['part_code'],
+                    'part_ca' => $part['part_ca'],
+                    'built_at' => $part['built_at'],
+                    'date_built' => $part['date_built']);
+            } else if ($part['part_code'] == "a2" || $part['part_code'] == "b2" || $part['part_code'] == "c2") {
+                $part_torsos[] = array('part_id' => $part['part_id'],
+                    'part_code' => $part['part_code'],
+                    'part_ca' => $part['part_ca'],
+                    'built_at' => $part['built_at'],
+                    'date_built' => $part['date_built']);
+            } else if ($part['part_code'] == "a3" || $part['part_code'] == "b3" || $part['part_code'] == "c3") {
+                $part_feet[] = array('part_id' => $part['part_id'],
+                    'part_code' => $part['part_code'],
+                    'part_ca' => $part['part_ca'],
+                    'built_at' => $part['built_at'],
+                    'date_built' => $part['date_built']);
+            } else {
+                continue;
+            }
+        }
+        $this->data['heads'] = $part_heads;
+        $this->data['torso'] = $part_torsos;
+        $this->data['feet'] = $part_feet;
+        $this->render();
+    }
+
+    public function item($id) {
+// view we want shown
+        $this->data['pagebody'] = 'justone';
+        $this->load->database();
+        $source = $this->inventory->getSinglePart($id);
+
+        $this->data['date_built'] = $source[0]['date_built'];
+        $this->data['part_id'] = $source[0]['part_id'];
+        $this->data['part_code'] = $source[0]['part_code'];
+        $this->data['part_ca'] = $source[0]['part_ca'];
+
+        $this->render();
+    }
+
+    public function buy() {
+        //$url = "https://umbrella.jlparry.com/work/mybuilds?key=".$key;
+        //need to get the actuall session KEY
+        $url = "https://umbrella.jlparry.com/work/buybox?key=3b7680";
+
+        $response = file_get_contents($url);
+        $parts = json_decode($response, true);
+        foreach ($parts as $part) {
+            $data = array(
+                'part_ca' => $part['id'],
+                'part_id' => $part['model'],
+                'part_code' => $part['piece'],
+                'built_at' => $part['plant'],
+                'date_built' => $part['stamp']
             );
 
-            $this->table->set_template($parms);
-
-            $list = array();
-            foreach ($parts as $part)
-            {
-                $list[] = array (
-                    'piece' => $part['piece'],
-                    'mug' => $part['mug'],
-                    'what' => $part['what']);
-            }
-
-            foreach($list as $x){
-                $cells[] = $this->parser->parse('_cell', (array) $x, true);
-            }
-
-            $rows = $this->table->make_columns($cells, 3);
-            $this->data['thetable'] = $this->table->generate($rows);
-            $this->data['pagebody'] = 'parts';
-            $this->render();
+            $this->Inventory->insertPart($data);
         }
+    }
 
-        public function item($id) {
-            // view we want shown
-            $this->data['pagebody'] = 'justone';
-            $source = $this->inventory->get($id);
-
-            // pull data from quotes data
-            $this->data['date'] = $source['date'];
-            $this->data['what'] = $source['what'];
-            $this->data['mug'] = $source['mug'];
-            $this->data['CA'] = $source['CA'];
-
-            $this->render();
+    public function build() {
+        $response = file_get_contents('https://umbrella.jlparry.com/work/mybuilds?key=3b7680');
+        $parts = json_decode($response, true);
+        foreach ($parts as $part) {
+            $data = array(
+                'part_ca' => $part['id'],
+                'part_id' => $part['model'],
+                'part_code' => $part['piece'],
+                'built_at' => $part['plant'],
+                'date_built' => $part['stamp']
+            );
+            $this->Inventory->insertPart($data);
         }
+    }
 
 }
--- application/views/parts.php
+++ application/views/parts.php
@@ -1,3 +1,56 @@
+<h2> Parts </h2>
+<div id="body">
+    <div id="homepageDash" >
+        <form action=<?php echo site_url("parts/part"); ?> method="post">
+            <table>
+                <thead> 
+                    <tr>
+                        <th><h3>Heads</h3></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        {heads}
+                        <td>
+                            <a href="/parts/{part_code}"><img src="assets/parts/{part_code}.jpeg" alt="Part Image" height="250" width="250">
+                        </td>
+                        {/heads}
+                    </tr>
 
-{thetable}
-<p> Available Parts </p>
+            </table>
+            <table>
+                <thead> 
+                    <tr>
+                        <th><h3>Torso</h3></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        {torso}
+                        <td>
+                            <a href="/parts/{part_code}"><img src="assets/parts/{part_code}.jpeg" alt="Part Image" height="250" width="250">
+                        </td>
+                        {/torso}
+                    </tr>
+
+            </table>
+            <table>
+                <thead> 
+                    <tr>
+                        <th><h3>Feet</h3></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        {feet}
+                        <td>
+                            <a href="/parts/{part_code}"><img src="assets/parts/{part_code}.jpeg" alt="Part Image" height="250" width="250">
+                        </td>
+                        {/feet}
+                    </tr>
+
+            </table>
+            <button id="{title}" type="button" onclick="build()" class="btn btn-success btn-sm" style="width: 200px">Build More Parts</button>
+            <button id="{title}" type="button" onclick="buy()" class="btn btn-danger btn-sm" style="width: 200px">Buy More Parts</button>
+    </div>
+</div>
